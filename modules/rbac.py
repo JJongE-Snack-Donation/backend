bdaca3c226939b28cf18d82824a29e2c6155aa60
dianin5 <xyg1940@cu.ac.kr>
2024-11-20 11:15:53 +0900

    jwt 토큰 발급 방식 수정

--- modules/rbac.py
+++ modules/rbac.py
@@ -1,16 +1,18 @@
 from flask import jsonify
-from flask_jwt_extended import verify_jwt_in_request, get_jwt
+from flask_jwt_extended import verify_jwt_in_request, get_jwt_identity
+from .database import find_user
 
 def role_required(required_role):
     # 특정 역할만 접근 가능하도록 제한
-    # 추후 어드민 관리페이지 추가 시 사용
     def decorator(func):
         def wrapper(*args, **kwargs):
             # JWT 인증 확인
             verify_jwt_in_request()
-            claims = get_jwt()
-            # 역할(role) 확인
-            if claims.get('role') != required_role:
+            username = get_jwt_identity()  # JWT에서 username만 가져옴
+
+            # 데이터베이스에서 역할 확인
+            user = find_user(username)
+            if not user or user['role'] != required_role:
                 return jsonify({"message": "Access forbidden: insufficient privileges"}), 403
             return func(*args, **kwargs)
         wrapper.__name__ = func.__name__
