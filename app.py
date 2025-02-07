9fa2f318ab2c138f8a0f7d92483d9e840821d989
dianin5 <xyg1940@cu.ac.kr>
2025-02-07 21:53:48 +0900

    cors 정책 업데이트

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
-
 from flask import Flask
-from modules import create_app
 from flask_cors import CORS
+from modules import create_app
 
 app = create_app()
-CORS(app)
+
+CORS(app, 
+     supports_credentials=True, 
+     origins=["http://localhost:3000"],
+     methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+     allow_headers=["Content-Type", "Authorization"]
+)
+
 if __name__ == '__main__':
     app.run(debug=True)
