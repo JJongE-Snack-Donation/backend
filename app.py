3a98076098fd456277a29ae91c7de48f1fc19a0e
dianin5 <xyg1940@cu.ac.kr>
2025-02-22 23:03:54 +0900

    그룹의 썸네일 출력 관련 url 변환, 프론트에서 접근할 수 있는 url 변환 용도

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask
+from flask import Flask, send_from_directory
 from flask_cors import CORS
 from modules import create_app
+import os
 
 app = create_app()
 
@@ -10,6 +11,16 @@ CORS(app,
      methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
      allow_headers=["Content-Type", "Authorization"]
 )
+
+@app.route('/images/<path:filename>')
+def serve_image(filename):
+    file_path = os.path.join(app.root_path, 'mnt', filename)
+    app.logger.debug(f"Requested image path: {file_path}")
+    if os.path.exists(file_path):
+        return send_from_directory(os.path.join(app.root_path, 'mnt'), filename)
+    else:
+        app.logger.error(f"File not found: {file_path}")
+        return "File not found", 404
 
 if __name__ == '__main__':
     app.run(debug=True)
